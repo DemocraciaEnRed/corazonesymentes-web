90ab22a02eb72ac15458995b9f9214e49e820f9d
mijail cohen <mijaelcohen@gmail.com>
2021-03-16 16:10:29 -0300

    integration gh pages

---
+++ scripts/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    '__sapper__/export/corazonesymentes',// <-- replace yourproject with your repo name
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/username/corazonesymentes-web.git',
+        user: {
+            name: 'Mijail Cohen',
+            email: 'mija@democracy.os'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)

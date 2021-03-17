09fe8947fd5f2e84a9b558dac5aeb3727a0a383c
fchpriani <sietedosfede@gmail.com>
2021-03-17 14:18:11 -0300

    refactor: use rollup setup instead webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,15 @@
-const tailwindcss = require("tailwindcss");
-
-// only needed if you want to purge
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.svelte", "./src/**/*.html"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+const tailwind = require('tailwindcss');
+const cssnano = require('cssnano');
+const postcssImport = require('postcss-import');
+const presetEnv = require('postcss-preset-env')({
+  features: {
+    'nesting-rules': true,
+  },
 });
 
-module.exports = {
-  plugins: [
-    tailwindcss("./tailwind.js"),
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [postcssImport, tailwind, presetEnv, cssnano]
+    : [postcssImport, tailwind, presetEnv];
 
-    // only needed if you want to purge
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
-};
+module.exports = { plugins };

959b88919e5e6f5e349c554695bd2a58635449f5
mijail cohen <mijaelcohen@gmail.com>
2021-03-25 11:19:57 -0300

    tag manager and footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -10,11 +10,11 @@
   }
 </style>
 <footer class="bg-gray-200 p-10">
-  <div class="container mx-auto fot">
-    <div class="grid md:grid-cols-3 grid-cols-1 gap-4">
+  <div class="container mx-auto fot justify-center align-center flex">
+    <div class="grid md:grid-cols-3 grid-cols-1 gap-4 ">
       <img class="logo" src="logo-circular.png" alt="corazones y mentes logo" />
       <div>
-        <div class="flex flex-col justify-center">
+        <div class="flex flex-col justify-center ">
           <small class="text-xs">Desarrollado por:</small>
           <img class="developers" src="developers2.png" alt="democracya en red, plataforma democratica fundacion fhc centro edelstein" />
         </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,12 +6,22 @@
 	import ForoDocente from '../components/ForoDocente.svelte'
 	import KitHerramientas from '../components/KitHerramientas.svelte'
 	import Footer from '../components/Footer.svelte';
+	import { onMount } from 'svelte';
+	onMount(()=>{
+		//Global site tag (gtag.js) - Google Analytics 
+		window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+  	gtag('config', 'G-LJWHDC5L57');
+	})
 </script>
 <svelte:head>
 	<title>Corazones Y Mentes</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJWHDC5L57"></script>
 </svelte:head>
+
 <Home/>
 <TodaLaData/>
 <Articulo/>

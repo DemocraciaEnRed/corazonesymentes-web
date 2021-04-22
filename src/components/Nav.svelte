03e434dcf9b232859b91f9e8395a88bb2ea87fbd
mijail cohen <mijaelcohen@gmail.com>
2021-04-22 15:47:09 -0300

    uglify nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -11,11 +11,7 @@
   }
   
 </script>
-
-<style>
-
-</style>
-<nav class="bg-white shadow">
+<nav class="bg-white shadow font-sans">
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -36,7 +32,7 @@
           </svg>
         </button>
       </div>
-      <div class="flex-1 flex items-center justify-between sm:items-stretch sm:justify-start">
+      <div class="flex-1 flex items-center justify-between sm:items-stretch">
         <div class="flex-shrink-0 flex items-center">
           <img class="block h-8 w-auto ml-10" src="logo-horizontal.png" alt="Corazones y mentes">
         </div>

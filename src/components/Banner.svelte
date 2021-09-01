06dd57c7314d0effec2556dbcedfd179f5742fd0
lietaju96 <julieta.garcia96@gmail.com>
2021-09-01 10:45:58 -0300

    -nueva seccion

---
+++ src/components/Banner.svelte
@@ -0,0 +1,54 @@
+<style>
+    .banner{
+        width: 100%;
+    }
+    .hide-on-mobile{
+        display: inherit;
+    }
+    .show-on-mobile{
+        display: none;
+    }
+    .show-on-tablet{
+        display:none
+    }
+    .button-se-voz{
+        bottom:23%;
+        right: 35%;
+    }
+    
+    
+    @media only screen and (max-width:768px){
+        .hide-on-tablet, .show-on-mobile{
+            display: none;
+        }
+        .show-on-tablet{
+            display: inherit;
+        }
+        .button-se-voz{
+        bottom:23%;
+        right: 25%;
+        }
+    }
+
+    @media only screen and (max-width:480px){
+        .show-on-tablet{
+            display: none;
+        }
+        .show-on-mobile{
+            display: inherit;
+        }
+        .button-se-voz{
+        bottom:12%;
+        right: 45%;
+        }
+        .button-se-voz{
+            font-size: 20px;
+        }
+    }
+</style>
+<div class="banner relative">
+    <img src="banner.jpg" class="hide-on-tablet" alt="Banner de la Campaña Se Voz">
+    <img src="banner-tablet.jpg" class="show-on-tablet" alt="Banner de la Campaña Se Voz">
+    <img src="banner-mobile.jpg" class="show-on-mobile" alt="Banner de la Campaña Se Voz">
+    <a href="https://preguntarparaacordar.typeform.com/to/VTI9VF52" class="bg-black button-se-voz	text-yellow-200	 text-2xl py-2 px-4 absolute" target="_blank">SUBÍ TU MEME</a>
+</div>
--- src/components/HeroSlider.svelte
+++ src/components/HeroSlider.svelte
@@ -1,20 +1,25 @@
 <script>
-  import Home from './Home.svelte'
+  	import Home from './Home.svelte'
 	import Carousel from '@beyonk/svelte-carousel';
-  import SosDocenteHero from './SosDocenteHero.svelte';
-  import { ChevronLeftIcon, ChevronRightIcon } from 'svelte-feather-icons'
+  	import SosDocenteHero from './SosDocenteHero.svelte';
+  	import Banner from './Banner.svelte'
+ 	import { ChevronLeftIcon, ChevronRightIcon } from 'svelte-feather-icons'
 </script>
 
 <Carousel  perPage={{ 800: 1, 500: 1 }} duration={500} >
 	<span class="control" slot="left-control">
 		<ChevronLeftIcon />
 	  </span>
+	<div class="slide-content">
+		<Banner/>
+	</div>
 	<div class="slide-content px-16">
 		<Home/>
 	</div>
 	<div class="slide-content px-16">
 		<SosDocenteHero/>
-	</div>	
+	</div>
+	
 	<span class="control" slot="right-control">
 		<ChevronRightIcon />
 	  </span>
